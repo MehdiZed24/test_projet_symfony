5dfe6874c31851cd5a9a6f7fd34e05c8c696d781
MehdiZed24 <mehdiziani1989@gmail.com>
2021-09-16 16:25:41 +0200

    Création du projet test & découverte de Twig

---
+++ src/Controller/BonjourController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BonjourController extends AbstractController
+{
+  /**
+   * @Route("/bonjour", name="bonjour")
+   */
+  public function index(): Response
+  {
+
+
+    $firstname = "Mehdi";
+    return $this->render('bonjour/index.html.twig', ['firstname' => $firstname]);
+  }
+}
